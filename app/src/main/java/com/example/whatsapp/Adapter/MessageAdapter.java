56558c9893352872ceb3fba77200a43f44d3c538
xiaosageee <1193287884@qq.com>
2019-11-22 23:09:29 +0800

    第二次提交，修改闪退的bug

---
+++ app/src/main/java/com/example/whatsapp/Adapter/MessageAdapter.java
@@ -0,0 +1,92 @@
+package com.example.whatsapp.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.whatsapp.Model.Chat;
+import com.example.whatsapp.Model.User;
+import com.example.whatsapp.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.List;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHolder> {
+
+    public static final int MSG_TYPE_LEFT = 0;
+    public static final int MSG_TYPE_Right = 1;
+    private Context mContext;
+    private List<Chat> mChat;
+    private String imageurl;
+
+    FirebaseUser firebaseUser;
+
+    public  MessageAdapter(Context mContext, List<Chat> mChat, String imageurl){
+        this.mChat = mChat;
+        this.mContext = mContext;
+        this.imageurl = imageurl;
+    }
+
+    @NonNull
+    @Override
+    public MessageAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == MSG_TYPE_Right){
+            View view = LayoutInflater.from(mContext).inflate(R.layout.chat_item_right, parent,false);
+            return new MessageAdapter.ViewHolder(view);
+        }else {
+            View view = LayoutInflater.from(mContext).inflate(R.layout.chat_item_left, parent,false);
+            return new MessageAdapter.ViewHolder(view);
+        }
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MessageAdapter.ViewHolder holder, int position) {
+        Chat chat = mChat.get(position);
+
+        holder.show_message.setText(chat.getMessage());
+
+        if (imageurl.equals("default")){
+            holder.profile_image.setImageResource(R.mipmap.ic_launcher);
+        }else {
+            Glide.with(mContext).load(imageurl).into(holder.profile_image);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mChat.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView show_message;
+        public ImageView profile_image;
+
+        public  ViewHolder(View itemView){
+            super(itemView);
+
+            show_message = itemView.findViewById(R.id.show_message);
+            profile_image = itemView.findViewById(R.id.profile_image);
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (mChat.get(position).getSender().equals(firebaseUser.getUid())){
+            return  MSG_TYPE_Right;
+        }else {
+            return MSG_TYPE_LEFT;
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/whatsapp/MessageActivity.java
@@ -0,0 +1,115 @@
+package com.example.whatsapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.bumptech.glide.Glide;
+import com.example.whatsapp.Model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MessageActivity extends AppCompatActivity {
+
+    CircleImageView profile_image;
+    TextView username;
+
+    FirebaseUser firebaseUser;
+    DatabaseReference reference;
+
+    ImageButton btn_send;
+    EditText text_send;
+
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        profile_image = findViewById(R.id.profile_image);
+        username = findViewById(R.id.username);
+        btn_send = findViewById(R.id.btn_send);
+        text_send = findViewById(R.id.text_send);
+
+        intent = getIntent();
+        final String userid = intent.getStringExtra("userid");
+
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = text_send.getText().toString();
+                if (!msg.equals("")){
+                    sendMessage(firebaseUser.getUid(),userid, msg);
+                }else{
+                    Toast.makeText(MessageActivity.this, "信息不能为空", Toast.LENGTH_SHORT).show();
+                }
+                text_send.setText("");
+                firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+            }
+
+        });
+
+
+        reference  = FirebaseDatabase.getInstance().getReference("Users").child(userid);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                username.setText(user.getUsername());
+                if (user.getImageURL().equals("default")){
+                    profile_image.setImageResource(R.mipmap.ic_launcher);
+                }else {
+                    Glide.with(MessageActivity.this).load(user.getImageURL()).into(profile_image);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void sendMessage(String sender, String receiver, String message){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("sender", sender);
+        hashMap.put("receiver", receiver);
+        hashMap.put("message", message);
+
+        reference.child("Chats").push().setValue(hashMap);
+    }
+}
